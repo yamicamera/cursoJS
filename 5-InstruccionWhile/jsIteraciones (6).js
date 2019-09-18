976fc37c0b18271b9fa15c31e230097658316113
yamicamera <54905595+yamicamera@users.noreply.github.com>
2019-09-18 16:54:03 -0300

    Add files via upload
    clase 18-09

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,26 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
 
+	for(var i=0; i < 5; i++) {
+		numero = parseInt( prompt("Ingrese un número"));
+	
+		//acumulador = acumuador + numero;
+		acumulador += numero;
 
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
+
+// var x = 10;
+//foo();
+//function foo() {
+//	var = 20;
+//	console.log(x);
+//}
+//console.log(x);
+// me graba primero el 20 y despues el 10
